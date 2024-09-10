f639cc7d2751446e737b52f9f30a44daded87c7d
MuhammadShamrokh <Muhammad.comp1996@gmail.com>
2024-09-10 14:13:40 +0300

    configuration class to configure limits-service was created

--- limits-service/src/main/java/com/muhammad/microservices/limits_service/controllers/LimitsController.java
+++ limits-service/src/main/java/com/muhammad/microservices/limits_service/controllers/LimitsController.java
@@ -1,15 +1,23 @@
 package com.muhammad.microservices.limits_service.controllers;
 
 import com.muhammad.microservices.limits_service.beans.Limit;
+import com.muhammad.microservices.limits_service.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
+    private Configuration configuration;
+
+    @Autowired
+    public LimitsController(Configuration configuration) {
+        this.configuration = configuration;
+    }
 
     @GetMapping(path = "/limits")
     public Limit retrieveLimits(){
-        return new Limit(1,999);
+        return new Limit(configuration.getMinimum(), configuration.getMaximum());
     }
 
 }
