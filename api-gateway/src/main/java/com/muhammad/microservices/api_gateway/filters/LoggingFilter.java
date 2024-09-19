ff6887c63af69e64474fa001191788a6877b5f28
MuhammadShamrokh <Muhammad.comp1996@gmail.com>
2024-09-19 13:55:33 +0300

    connected services to distributed tracing server (zipkin)

---
+++ api-gateway/src/main/java/com/muhammad/microservices/api_gateway/filters/LoggingFilter.java
@@ -0,0 +1,19 @@
+package com.muhammad.microservices.api_gateway.filters;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+@Component
+public class LoggingFilter implements GlobalFilter {
+    private Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Path of the received Request: {}",exchange.getRequest().getPath());
+
+        return chain.filter(exchange);
+    }
+}
